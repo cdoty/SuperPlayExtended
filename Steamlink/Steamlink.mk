0f7e5695114db3d402b0ae76bf8d9fe2d3da3d24
Charles Doty <cdoty@users.noreply.github.com>
2021-11-08 17:02:08 -0600

    Steamlink makefile updates
    Update Steamlink makefiles.

--- Steamlink/Steamlink.mk
+++ Steamlink/Steamlink.mk
@@ -2,31 +2,21 @@ PATH_SDK = $(STEAMLINK_DIR)
 PATH_LIB = $(PATH_SDK)/lib
 PATH_USR = $(PATH_SDK)/usr
 PATH_TOOLCHAIN = $(PATH_SDK)/toolchain
+SYSROOT	= $(PATH_SDK)/rootfs
 
 CROSS_COMPILE = $(PATH_TOOLCHAIN)/bin/armv7a-cros-linux-gnueabi-
-CC = $(CROSS_COMPILE)gcc
-CXX = $(CROSS_COMPILE)g++
+CC = $(CROSS_COMPILE)gcc --sysroot=$(SYSROOT)
+CXX = $(CROSS_COMPILE)g++ --sysroot=$(SYSROOT)
 AR = $(CROSS_COMPILE)ar
 AS = $(CROSS_COMPILE)as
-LD = $(CROSS_COMPILE)gcc
+LD = $(CROSS_COMPILE)ld
 OBJCOPY = $(CROSS_COMPILE)objcopy
 STRIP = $(CROSS_COMPILE)strip
 RANLIB = $(CROSS_COMPILE)ranlib
 
-COMMON_FLAGS = -mcpu=cortex-a9 -mthumb \
-	-fvisibility=hidden -fdata-sections -ffunction-sections \
-	-Wno-parentheses -U_FORTIFY_SOURCE \
-	-I$(PATH_SDK)/include -I$(PATH_USR)/include $(INCLUDES) -DROKU
+COMMON_FLAGS = -I$(SYSROOT)/usr/include $(INCLUDES) -DSTEAMLINK
 
 CFLAGS = -std=c99 $(COMMON_FLAGS)
 CXXFLAGS = -std=c++14 -fvisibility-inlines-hidden $(COMMON_FLAGS)
 
-PATH_LIBS = \
-	-L $(PATH_LIB) -L$(PATH_USR)/lib
-
-LDFLAGS = -Wl,--gc-sections -Wl,--copy-dt-needed-entries \
-	-Wl,--allow-shlib-undefined -Wl,--no-as-needed \
-	$(PATH_LIBS) -lc -lpthread -lrt -ldl -lstdc++
-
-UTIL_MKSQUASHFS = ~/Roku/bin/mksquashfs_roku
-UTIL_PAXCTL = ~/Roku/bin/paxctl
+LINK_OPTIONS = "-Wl,-rpath-link,$MARVELL_ROOTFS/lib -Wl,-rpath-link,$MARVELL_ROOTFS/usr/lib -Wl,-rpath-link,$MARVELL_ROOTFS/usr/lib/arm-linux-gnueabihf"
--- Steamlink/rules.mk
+++ Steamlink/rules.mk
@@ -1,9 +1,7 @@
-PATH_PACKAGE ?= $(ROKU)/package
-PATH_MANIFEST ?= $(ROKU)/assets/manifest
-PATH_ASSETS ?= $(ROKU)/assets
-PATH_COMPONENTS ?= $(ROKU)/components
+PATH_PACKAGE ?= $(STEAMLINK)/package
+PATH_MANIFEST ?= $(STEAMLINK)/assets/manifest
+PATH_ASSETS ?= $(STEAMLINK)/assets
 
-COMPONENTS := $(wildcard $(PATH_COMPONENTS)/*)
 ASSETS := $(wildcard $(PATH_ASSETS)/*)
 DEPENDS := $(OBJECTS:%.o=%.d)
 
@@ -18,21 +16,19 @@ clean:
 	rm -f $(OBJECTS) $(DEPENDS)
 	rm -rf $(PATH_PACKAGE)
 env-check:
+	export LD_LIBRARY_PATH="$(PATH_TOOLCHAIN)/lib"
 ifeq (, $(wildcard $(CXX)))
-	$(error CXX is not set, did you forget to include Roku4.mk?)
+	$(error CXX is not set, did you forget to include Stea.mk?)
 endif
 ifndef BINARY
 	$(error Must define BINARY, name of your application.)
-endif
-ifndef PATH_MANIFEST
-	$(error Must define PATH_MANIFEST, file containing information about your application.)
 endif
 
 $(PATH_PACKAGE)/$(BINARY): $(OBJECTS)
 	@echo "######################################################################"
 	@echo "#    BUILDING $(PATH_PACKAGE)/$(BINARY)"
 	@echo "######################################################################"
-	@echo "SOURCES: $(C_SRCS)$(CC_SRCS)$(CXX_SRCS)"
+	@echo "SOURCES: $(CXX_SRCS)"
 	rm -rf $(PATH_PACKAGE)
 	mkdir -p $(PATH_PACKAGE)
 	$(LD) $(LDFLAGS) $(LIBRARIES) -lz $^ -o $(PATH_PACKAGE)/$(BINARY)
@@ -42,17 +38,6 @@ $(PACKAGE): $(PATH_PACKAGE)/$(BINARY) $(ASSETS) $(COMPONENTS)
 	@echo "######################################################################"
 	@echo "#    PACKAGING $(PACKAGE)"
 	@echo "######################################################################"
-	rm -f $(PACKAGE)
-	rm -rf $(PATH_PACKAGE)/source
-	cp -a $(PATH_ASSETS) $(PATH_PACKAGE)/source
-	rm -f `find $(PATH_PACKAGE)/source -name manifest`
-	cp -f $(PATH_MANIFEST) $(PATH_PACKAGE)
-	cp -f $(RESOURCE_PATH)/icon-246x140.png $(PATH_PACKAGE)/source
-	cp -f $(RESOURCE_PATH)/icon-290x218.png $(PATH_PACKAGE)/source
-	cp -f $(RESOURCE_PATH)/splash-720p.png $(PATH_PACKAGE)/source
-	cp -f $(RESOURCE_PATH)/splash-480p.png $(PATH_PACKAGE)/source
-	chmod -R 755 $(PATH_PACKAGE)
-	$(UTIL_MKSQUASHFS) $(PATH_PACKAGE) $(PACKAGE) -force-uid 499 -force-gid 499 -noappend -comp gzip
 ifdef HOST_DIR	
 	cp $(PACKAGE) $(HOST_DIR)
 endif
